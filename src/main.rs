389212de7aa8307bbe4875143d1930aac3a34354
John Erickson <john.t.erickson@gmail.com>
2023-01-28 00:18:27 -0800

    all - but needs to be checked

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, fmt::Display};
 
-type Num=isize;
+type Num=f64;
 
 #[derive(Debug, Clone)]
 struct State {
     fours_used: usize,
-    path: VecDeque<Action>,
+    path: VecDeque<(Action, Num)>,
     stack: VecDeque<Num>
 }
 
@@ -17,6 +17,33 @@ impl State {
             stack: VecDeque::new(),
         }
     }
+
+    fn fmt(f: &mut std::fmt::Formatter<'_>, actions: &mut VecDeque<Action>) -> std::fmt::Result {
+        match actions.pop_back() {
+            Some(Action::Abs) => {
+                write!(f, "|")?;
+                Self::fmt(f, actions)?;
+                write!(f, "|")?;
+            },
+            Some(Action::Add) => {
+                write!(f, "(")?;
+                Self::fmt(f, actions)?;
+                write!(f, ")+(")?;
+                Self::fmt(f, actions)?;
+                write!(f, ")")?;
+            }
+            Some(_) => todo!(),
+            None => todo!(),
+        }
+        Ok(())
+    }
+}
+
+impl Display for State {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut path : VecDeque<_> = self.path.iter().map(|(a, _)| *a).collect();
+        State::fmt(f, &mut path)
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -26,14 +53,28 @@ enum Action {
     Mul,
     Div,
     SquareRoot,
-    FourthRoot,
+    Root,
     Push4,
-    Factorial,
     Power,
+    Floor,
+    Ceiling,
+    Abs
 }
 
 impl Action {
-    const VALUES: [Self; 9] = [Self::Add, Self::Sub, Self::Mul, Self::Div, Self::Push4, Self::SquareRoot, Self::FourthRoot, Self::Factorial, Self::Power];
+    const VALUES: [Self; 11] = [
+        Self::Push4,
+        Self::Add,
+        Self::Sub,
+        Self::Mul,
+        Self::Div,
+        Self::Power,
+        Self::Floor,
+        Self::Ceiling,
+        Self::Abs,
+        Self::SquareRoot,
+        Self::Root
+    ];
 
     fn apply(&self, s: &mut State) -> Option<Num> {
         match self {
@@ -64,7 +105,7 @@ impl Action {
                 if s.stack.len() >= 2 {
                     let right = s.stack.pop_back().unwrap();
                     let left = s.stack.pop_back().unwrap();
-                    if right == 0 || left % right != 0 {
+                    if right == 0.0 {
                         s.stack.push_back(left);
                         s.stack.push_back(right);
                         None
@@ -78,84 +119,73 @@ impl Action {
             Action::SquareRoot => {
                 if s.stack.len() >= 1 {
                     let n = s.stack.pop_back().unwrap();
-                    let r = (n as f64).sqrt() as Num;
-                    if n > 1 && r * r == n {
-                        Some(r)
-                    } else {
-                        s.stack.push_back(n);
-                        None
-                    }
+                    Some(n.sqrt())
                 } else {
                     None
                 }
             },
-            Action::FourthRoot => {
-                if s.stack.len() >= 1 {
-                    let n = s.stack.pop_back().unwrap();
-                    let r = (n as f64).sqrt().sqrt() as Num;
-                    if n > 1 && r * r * r * r == n {
-                        Some(r)
-                    } else {
-                        s.stack.push_back(n);
-                        None
-                    }
-                } else {
-                    None
-                }
-            }
             Action::Push4 => {
                 if s.fours_used < 4 {
                     s.fours_used += 1;
-                    Some(4)
-                } else {
-                    None
-                }
-            },
-            Action::Factorial => {
-                if s.stack.len() >= 1 {
-                    let mut n = s.stack.pop_back().unwrap();
-                    if n > 2 && n < 10 {
-                        for i in 2..n {
-                            n *= i;
-                        }
-                        Some(n)
-                    } else {
-                        s.stack.push_back(n);
-                        None
-                    }
+                    Some(4.0)
                 } else {
                     None
                 }
             },
             Action::Power => {
-                let ipower = |base: Num, power: Num| {
-                    if power == 0 {
-                        Some(1)
-                    } else if power > 0 {
-                        let mut p = 1 as Num;
-                        for _ in 0..power {
-                            if let Some(pp) = p.checked_mul(base) {
-                                p = pp;
-                            } else {
-                                return None;
-                            }
-                        }
-                        Some(p)
-                    } else {
-                        None
-                    }
-                };
-
                 if s.stack.len() >= 2 {
                     let power = s.stack.pop_back().unwrap();
                     let base = s.stack.pop_back().unwrap();
-
-                    if let Some(p) = ipower(base, power) {
-                        Some(p)
-                    } else {
-                        s.stack.push_back(base);
-                        s.stack.push_back(power);
+                    Some(base.powf(power))
+                } else {
+                    None
+                }
+            },
+            Action::Root => {
+                if s.stack.len() >= 2 {
+                    let power = s.stack.pop_back().unwrap();
+                    let base = s.stack.pop_back().unwrap();
+                    Some(base.powf(1.0/power))
+                } else {
+                    None
+                }
+            },
+            Action::Floor => {
+                if s.stack.len() >= 1 {
+                    let n = s.stack.pop_back().unwrap();
+                    let closest = n.round();
+                    if (closest - n).abs() < 0.001 {
+                        s.stack.push_back(n);
                         None
+                    } else {
+                        Some(n.floor())
+                    }
+                } else {
+                    None
+                }
+            },
+            Action::Ceiling => {
+                if s.stack.len() >= 1 {
+                    let n = s.stack.pop_back().unwrap();
+                    let closest = n.round();
+                    if (closest - n).abs() < 0.001 {
+                        s.stack.push_back(n);
+                        None
+                    } else {
+                        Some(n.floor())
+                    }
+                } else {
+                    None
+                }
+            },
+            Action::Abs =>  {
+                if s.stack.len() >= 1 {
+                    let n = s.stack.pop_back().unwrap();
+                    if n >= 0.0 {
+                        s.stack.push_back(n);
+                        None
+                    } else {
+                        Some(n.abs())
                     }
                 } else {
                     None
@@ -165,44 +195,65 @@ impl Action {
     }
 }
 
-fn explore(s: &mut State, found: &mut Vec<Option<State>>, depth: usize) {
+fn explore(s: &mut State, found: &mut Vec<Option<State>>, depth: usize) -> bool {
 
     assert!(depth < 50);
 
     let prev_state = s.clone();
+    // let prev_result = prev_state.stack.back();
 
     for a in &Action::VALUES {
         if let Some(result) = a.apply(s) {
 
-            // println!("{:?}", &s.path);
+            if !result.is_finite() {
+                *s = prev_state.clone();
+                continue;
+            }
+
+            // println!("{:?} = {}", &s.path, result);
+            s.path.push_back((*a, /* prev_state.stack.clone(), */ result));
             s.stack.push_back(result);
-            s.path.push_back(*a);
 
             // check to see if we are done
             if s.fours_used == 4 && s.stack.len() == 1 {
-                if let Ok(result) = usize::try_from(result) {
-                    if let Some(found) = found.get_mut(result) {
+                let closest = result.round();
+                if closest > 0.0 && (result - closest).abs() < 0.0000001 {
+                    if let Some(slot) = found.get_mut(closest as usize) {
                         // println!("{:?} = {}", &s.path, result);
-                        if found.is_none() {
+                        if slot.is_none() || slot.as_ref().unwrap().path.len() > s.path.len() {
                             // dbg!(result, &s.path);
-                            println!("{:?} = {}", &s.path, result);
-                            *found = Some(s.clone());
+
+                            *slot = Some(s.clone());
+
+                            let total = found.len();
+                            let found_already = found.iter().filter(|x| x.is_some()).count();
+
+                            println!("{:?} = {} [{} of {} found]", &s.path, result, found_already, total);
+
+                            if found_already == total {
+                                return true;
+                            }
                         }
                     }
                 }
-            } else if result.abs() < 1_000_000 {
+            } else if /*prev_result.is_none() ||  (prev_result.unwrap() - result).abs() > 0.01 &&*/ depth < 25 {
+            // } else if depth < 20 {
                 // otherwise recurse
-                explore(s, found, depth + 1)
+                if explore(s, found, depth + 1) {
+                    return true;
+                }
             }
 
             *s = prev_state.clone();
         }
     }
+
+    false
 }
 
 fn main() {
     let mut s = State::new();
-    let mut found = vec![None; 110];
+    let mut found = vec![None; 101];
     explore(&mut s, &mut found, 0);
 
     for (i, s) in found.iter().enumerate() {
