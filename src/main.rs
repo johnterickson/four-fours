0244bb694bdb6983573387aa88cd6cbbc0db2959
John Erickson <john.t.erickson@gmail.com>
2023-01-29 14:06:40 -0800

    all and concise

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,87 @@ struct State {
     stack: VecDeque<Num>
 }
 
+#[derive(Debug)]
+struct Expression {
+    action: Action,
+    args: Vec<Expression>
+}
+
+impl Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>, has_parens: bool, parent_action: Option<Action>) -> std::fmt::Result {
+        match self.action {
+            Action::Push4 => {
+                write!(f, "4")?;
+            }
+            Action::Abs => {
+                write!(f, "|")?;
+                self.args[0].fmt(f, true, Some(self.action))?;
+                write!(f, "|")?;
+            },
+            Action::FourthRoot => {
+                write!(f, "∜(")?;
+                self.args[0].fmt(f, true, Some(self.action))?;
+                write!(f, ")")?;
+            }
+            Action::SquareRoot => {
+                write!(f, "√(")?;
+                self.args[0].fmt(f, true, Some(self.action))?;
+                write!(f, ")")?;
+            }
+            Action::Floor => {
+                write!(f, "⌊")?;
+                self.args[0].fmt(f, true, Some(self.action))?;
+                write!(f, "⌋")?;
+            }
+            Action::Ceiling => {
+                write!(f, "⌈")?;
+                self.args[0].fmt(f, true, Some(self.action))?;
+                write!(f, "⌉")?;
+            }
+            Action::Add | Action::Sub | Action::Mul | Action::Div | Action::Power => {
+                let needs_parens = parent_action.is_some() && parent_action != Some(self.action);
+                if needs_parens && !has_parens {
+                    write!(f, "(")?;
+                }
+                self.args[0].fmt(f, false, Some(self.action))?;
+                write!(f, "{}",
+                    match self.action {
+                        Action::Add => '+',
+                        Action::Sub => '-',
+                        Action::Mul => '*',
+                        Action::Div => '/',
+                        Action::Power => '^',
+                        _ => panic!(),
+                    })?;
+                self.args[1].fmt(f, false, Some(self.action))?;
+                if needs_parens && !has_parens {
+                    write!(f, ")")?;
+                }
+            }
+            Action::Root => {
+                if !has_parens {
+                    write!(f, "(")?;
+                }
+                write!(f, "(")?;
+                self.args[0].fmt(f, true, Some(self.action))?;
+                write!(f, ")^(")?;
+                self.args[1].fmt(f, true, Some(self.action))?;
+                write!(f, ")")?;
+                if !has_parens {
+                    write!(f, ")")?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Display for Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.fmt(f, true, None)
+    }
+}
+
 impl State {
     fn new() -> Self {
         State {
@@ -18,41 +99,46 @@ impl State {
         }
     }
 
-    fn fmt(f: &mut std::fmt::Formatter<'_>, actions: &mut VecDeque<Action>) -> std::fmt::Result {
-        match actions.pop_back() {
-            Some(Action::Abs) => {
-                write!(f, "|")?;
-                Self::fmt(f, actions)?;
-                write!(f, "|")?;
-            },
-            Some(Action::Add) => {
-                write!(f, "(")?;
-                Self::fmt(f, actions)?;
-                write!(f, ")+(")?;
-                Self::fmt(f, actions)?;
-                write!(f, ")")?;
+    fn make_expression(&self) -> Expression {
+        let mut stack = VecDeque::new();
+        // println!("make_expression {:?}", &self);
+        for (a, _) in &self.path {
+            // println!("{:?} {:?}", a, &stack);
+            match a {
+                Action::Add | Action::Sub | Action::Mul | Action::Div | Action::Power | Action::Root => {
+                    let first = stack.pop_back().unwrap();
+                    let second = stack.pop_back().unwrap();
+                    stack.push_back(Expression { action: *a, args: vec![first, second]});
+                }
+                Action::SquareRoot | Action::FourthRoot | Action::Floor | Action::Ceiling | Action::Abs => {
+                    let first = stack.pop_back().unwrap();
+                    stack.push_back(Expression { action: *a, args: vec![first]});
+                }
+                Action::Push4 => stack.push_back(Expression { action: *a, args: Vec::new() }),
             }
-            Some(_) => todo!(),
-            None => todo!(),
         }
-        Ok(())
+
+        assert_eq!(1, stack.len());
+
+        stack.pop_back().unwrap()
     }
 }
 
-impl Display for State {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut path : VecDeque<_> = self.path.iter().map(|(a, _)| *a).collect();
-        State::fmt(f, &mut path)
-    }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    // use super::*;
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Action {
     Add,
     Sub,
     Mul,
     Div,
     SquareRoot,
+    FourthRoot,
     Root,
     Push4,
     Power,
@@ -62,7 +148,7 @@ enum Action {
 }
 
 impl Action {
-    const VALUES: [Self; 11] = [
+    const VALUES: [Self; 12] = [
         Self::Push4,
         Self::Add,
         Self::Sub,
@@ -73,6 +159,7 @@ impl Action {
         Self::Ceiling,
         Self::Abs,
         Self::SquareRoot,
+        Self::FourthRoot,
         Self::Root
     ];
 
@@ -87,9 +174,9 @@ impl Action {
             }
             Action::Sub => {
                 if s.stack.len() >= 2 {
-                    let right = s.stack.pop_back().unwrap();
-                    let left = s.stack.pop_back().unwrap();
-                    Some(left - right)
+                    let first = s.stack.pop_back().unwrap();
+                    let second = s.stack.pop_back().unwrap();
+                    Some(first - second)
                 } else {
                     None
                 }
@@ -103,14 +190,14 @@ impl Action {
             }
             Action::Div => {
                 if s.stack.len() >= 2 {
-                    let right = s.stack.pop_back().unwrap();
-                    let left = s.stack.pop_back().unwrap();
-                    if right == 0.0 {
-                        s.stack.push_back(left);
-                        s.stack.push_back(right);
+                    let first = s.stack.pop_back().unwrap();
+                    let second = s.stack.pop_back().unwrap();
+                    if second == 0.0 {
+                        s.stack.push_back(second);
+                        s.stack.push_back(first);
                         None
                     } else {
-                        Some(left / right)
+                        Some(first / second)
                     }
                 } else {
                     None
@@ -118,8 +205,15 @@ impl Action {
             },
             Action::SquareRoot => {
                 if s.stack.len() >= 1 {
-                    let n = s.stack.pop_back().unwrap();
-                    Some(n.sqrt())
+                    Some(s.stack.pop_back().unwrap().sqrt())
+                } else {
+                    None
+                }
+            },
+            Action::FourthRoot => {
+                if s.fours_used < 4 && s.stack.len() >= 1 {
+                    s.fours_used += 1;
+                    Some(s.stack.pop_back().unwrap().sqrt().sqrt())
                 } else {
                     None
                 }
@@ -134,8 +228,8 @@ impl Action {
             },
             Action::Power => {
                 if s.stack.len() >= 2 {
-                    let power = s.stack.pop_back().unwrap();
                     let base = s.stack.pop_back().unwrap();
+                    let power = s.stack.pop_back().unwrap();
                     Some(base.powf(power))
                 } else {
                     None
@@ -143,8 +237,8 @@ impl Action {
             },
             Action::Root => {
                 if s.stack.len() >= 2 {
-                    let power = s.stack.pop_back().unwrap();
                     let base = s.stack.pop_back().unwrap();
+                    let power = s.stack.pop_back().unwrap();
                     Some(base.powf(1.0/power))
                 } else {
                     None
@@ -195,7 +289,7 @@ impl Action {
     }
 }
 
-fn explore(s: &mut State, found: &mut Vec<Option<State>>, depth: usize) -> bool {
+fn explore(s: &mut State, found: &mut Vec<Option<(State,usize)>>, depth: usize) -> bool {
 
     assert!(depth < 50);
 
@@ -217,21 +311,28 @@ fn explore(s: &mut State, found: &mut Vec<Option<State>>, depth: usize) -> bool 
             // check to see if we are done
             if s.fours_used == 4 && s.stack.len() == 1 {
                 let closest = result.round();
-                if closest > 0.0 && (result - closest).abs() < 0.0000001 {
-                    if let Some(slot) = found.get_mut(closest as usize) {
+                if closest >= 0.0 && (result - closest).abs() < 0.0000001 {
+                    let closest = closest as usize;
+                    if let Some(slot) = found.get_mut(closest) {
                         // println!("{:?} = {}", &s.path, result);
-                        if slot.is_none() || slot.as_ref().unwrap().path.len() > s.path.len() {
+
+                        if slot.is_none() || slot.as_ref().unwrap().0.path.len() >= s.path.len() {
                             // dbg!(result, &s.path);
 
-                            *slot = Some(s.clone());
+                            let e = format!("{}", s.make_expression());
 
-                            let total = found.len();
-                            let found_already = found.iter().filter(|x| x.is_some()).count();
+                            if slot.is_none() || e.len() < slot.as_ref().unwrap().1 {
 
-                            println!("{:?} = {} [{} of {} found]", &s.path, result, found_already, total);
+                                *slot = Some((s.clone(), e.len()));
 
-                            if found_already == total {
-                                return true;
+                                let total = found.len();
+                                let found_already = found.iter().filter(|x| x.is_some()).count();
+
+                                println!("{:?} = {} = {} [{} of {} found]", &s.path, &e, closest, found_already, total);
+
+                                if found_already == total {
+                                    return true;
+                                }
                             }
                         }
                     }
@@ -257,7 +358,11 @@ fn main() {
     explore(&mut s, &mut found, 0);
 
     for (i, s) in found.iter().enumerate() {
-        println!("{} = {:?}", i, s.as_ref().map(|s| &s.path));
+        if let Some(s) = s.as_ref() {
+            println!("{} = {}", i, s.0.make_expression());
+        } else {
+            println!("{} = ?", i);
+        }
     }
 
     println!("Found {} of [0,100]", (0..=100).filter(|i| found[*i].is_some()).count());
